790a32eac303379389d0c0c93bca7f1b4e079651
Guillaume P <gcconantc@gmail.com>
2024-01-11 07:57:19 +0100

    clean build

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # PoulpeEdit
 
 PoulpeEdit is the level editor that comes with [PoulpeEngine](https://github.com/galliume/poulpeEngine)
+
+## Build it
+
+### GTK for Windows
+[Download GTK](https://gitlab.gnome.org/GNOME/gtk)
+
+Then in the developer command prompt shell load vcvars64.bat (should be here:)
+```
+C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat
+```
+
+Then build it from the source (update ${PoulpeEditFolder} accordingly)
+```
+meson setup --prefix=${PoulpeEditFolder}/vendor/gtk _build -Dmedia-gstreamer=disabled -Dwin32-backend=true -Dvulkan=disabled
+meson compile -C _build
+meson install -C _build
+```
+
+Then build PoulpeEdit
+```
+chmod +x ./bin/plpEdit.sh
+./bin/plpEdit.sh --config --build
+```
--- bin/plpEdit.sh
+++ bin/plpEdit.sh
@@ -2,7 +2,6 @@
 
 set -o errexit -o pipefail -o noclobber -o nounset
 
-buildGTK=0
 configPoulpeEdit=0
 buildPoulpeEdit=0
 
@@ -12,19 +11,13 @@ showusage() {
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     echo "--config           CMake config"
     echo "--buildPoulpeEdit  Build PoulpeEdit"
-    echo "--buildGTK         Build GTK (with meson)"
     echo "--help             Show this help"
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 }
 
 while [[ $# -gt 0 ]]; do
     argument="$1"
-    echo $argument
     case "$argument" in
-      --buildGTK)
-        buildGTK=1
-        shift
-        ;;
       -c|--config)
         configPoulpeEdit=1
         shift
@@ -46,22 +39,13 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-if [[ buildGTK -eq 1 ]]
-then
-    pushd ./vendor/gtk/
-    meson setup --prefix=E:/vendor/gtk/release _build -Dmedia-gstreamer=disabled -Dwin32-backend=true -Dvulkan=disabled
-    meson compile -C _build
-    meson install -C _build
-    popd
-fi
-
 if [[ configPoulpeEdit -eq 1 ]]
 then
     rm ./build -rf
-    cmake -B ./build/Debug
+    cmake -B ./build/
 fi
 
 if [[ buildPoulpeEdit -eq 1 ]]
 then
-    cmake --build ./build/Debug -j8
+    cmake --build ./build/ -j8
 fi
