59de2094c0953db9e871fd8d7805dbd8f00d8195
Guillaume PERCEPIED <gcconantc@gmail.com>
2024-01-11 07:59:40 +0100

    Init building PoulpeEdit (#1)
     build PoulpeEdit for Windows

--- bin/plpEdit.sh
+++ bin/plpEdit.sh
@@ -1,4 +1,51 @@
-clang `pkg-config --cflags gtk4` \
-    -o ./build/poulpeEdit \
-    ./src/main.c \
-    `pkg-config --libs gtk4`
+#!/usr/bin/env bash
+
+set -o errexit -o pipefail -o noclobber -o nounset
+
+configPoulpeEdit=0
+buildPoulpeEdit=0
+
+showusage() {
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "~~~~~~ PoulpeEdit ~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "--config           CMake config"
+    echo "--buildPoulpeEdit  Build PoulpeEdit"
+    echo "--help             Show this help"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+}
+
+while [[ $# -gt 0 ]]; do
+    argument="$1"
+    case "$argument" in
+      -c|--config)
+        configPoulpeEdit=1
+        shift
+        ;;
+      -b|--build)
+        buildPoulpeEdit=1
+        shift
+        ;;
+      -h|--help)
+        showusage
+        exit 0
+        ;;
+      *|-*|--*)
+        echo "Unknown option $argument"
+
+        showusage
+        exit 1
+        ;;
+    esac
+done
+
+if [[ configPoulpeEdit -eq 1 ]]
+then
+    rm ./build -rf
+    cmake -B ./build/
+fi
+
+if [[ buildPoulpeEdit -eq 1 ]]
+then
+    cmake --build ./build/ -j8
+fi
